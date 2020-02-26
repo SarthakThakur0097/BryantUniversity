f41f385321028e254223fee8b26ff93b4c37469d
Sarthak <sarthakthakur0097@gmail.com>
2020-02-26 12:41:19 -0500

    added list for semesters

--- BryantUniversity/BryantUniversity/ViewModels/SemesterDetailsViewModel.cs
+++ BryantUniversity/BryantUniversity/ViewModels/SemesterDetailsViewModel.cs
@@ -16,8 +16,18 @@ namespace BryantUniversity.ViewModels
         public string UrlSlug { get; set; }
 
         public IList<SemesterDetails> semesterDetails = new List<SemesterDetails>();
+
+        public List<string> SemesterDates;
+
         public SemesterDetailsViewModel()
-        { 
+        {
+            SemesterDates = new List<string>()
+            {
+                "Fall 2020",
+                "Spring 2020",
+                "Fall 2019",
+                "Spring 2019"
+            };
         }
     }
 }
