20f54ac19055bea27d18bc286a857d4f019d414b
Sarthak <sarthakthakur0097@gmail.com>
2020-02-28 15:34:29 -0500

    in progress

---
+++ BryantUniversity/BryantUniversity/Repo/CoursesRepo.cs
@@ -0,0 +1,43 @@
+﻿using BryantUniversity.Data;
+using BryantUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BryantUniversity.Repo
+{
+    public class CoursesRepo
+    {
+        private Context _context;
+
+        public CoursesRepo(Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> GetAllCourses()
+        {
+            return _context.Courses.ToList();
+
+        }
+
+        public Course GetById(int id)
+        {
+            return _context.Courses.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Insert(Course course)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
+
+        public void Update(Course course)
+        {
+            _context.Courses.Attach(course);
+            _context.Entry(course).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ BryantUniversity/BryantUniversity/ViewModels/CourseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BryantUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BryantUniversity.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+        public string CourseTitle { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; }
+        public string Level { get; set; }
+
+        public IList<Course> Courses { get; set; }
+        public CourseViewModel() { }
+    }
+}
