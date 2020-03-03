a5d4485e81a799c3c3541c5c378a232efcbb428e
Sarthak <sarthakthakur0097@gmail.com>
2020-03-03 11:44:37 -0500

    Merging to fix apicontroller

--- BryantUniversity/BryantUniversity/Models/FacultyDepartment.cs
+++ BryantUniversity/BryantUniversity/Models/FacultyDepartment.cs
@@ -7,8 +7,19 @@ namespace BryantUniversity.Models
 {
     public class FacultyDepartment
     {
+        public FacultyDepartment() { }
+        public FacultyDepartment (User teacher, Department department)
+        {
+            Faculty = teacher;
+            Department = department; 
+        }
         public int Id { get; set; }
         public int HoursWorked { get; set; }
-        public List<User> Faculty { get; set; }
+        public int UserId { get; set; }
+        public User Faculty { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+
     }
 }
