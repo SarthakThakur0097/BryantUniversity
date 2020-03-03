82b130185ffcc464d6562eee66ca4fb21d8b1097
Sarthak <sarthakthakur0097@gmail.com>
2020-03-02 19:19:54 -0500

    Minor Updates

--- BryantUniversity/BryantUniversity/Models/Building.cs
+++ BryantUniversity/BryantUniversity/Models/Building.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,8 @@ namespace BryantUniversity.Models
         public Building() { }
 
         public int Id { get; set; }
+        [Required]
+        public string BuildingName { get; set; }
         public int Capacity { get; set; }
         public List<Room> Rooms { get; set; }
 
