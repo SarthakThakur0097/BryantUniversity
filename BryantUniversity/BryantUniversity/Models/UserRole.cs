55b54ab4ffc44b6927a9be802a035aed5ac9aecf
Sarthak <sarthakthakur0097@gmail.com>
2020-03-06 14:58:26 -0500

    Added FK navigation properties all models

--- BryantUniversity/BryantUniversity/Models/UserRole.cs
+++ BryantUniversity/BryantUniversity/Models/UserRole.cs
@@ -12,9 +12,8 @@
             
         public int Id { get; set; }
         public int UserId { get; set; }
-        public int RoleId { get; set; }
-
         public User User {get; set; }
+        public int RoleId { get; set; }
         public Role Role { get; set; }
     }
 }
