c1de674d4b55f8811741972c1a0b53e72395b15e
Sarthak <sarthakthakur0097@gmail.com>
2020-04-26 15:47:26 -0400

    Recreated helper and rdscontext class

---
+++ BryantUniversity/BryantUniversity/Data/RDSContext.cs
@@ -0,0 +1,18 @@
+﻿using BryantUniversity.Models;
+using System.Data.Entity;
+
+namespace BryantUniversity.Data
+{
+    public class RDSContext : DbContext
+    {
+        public RDSContext()
+          : base(Helpers.GetRDSConnectionString())
+        {
+        }
+
+        public static RDSContext Create()
+        {
+            return new RDSContext();
+        }
+    }
+}
