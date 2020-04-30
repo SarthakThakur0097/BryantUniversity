a6b3d4c3843832848c617f5b7cbe4785e2b7b85d
Sarthak <sarthakthakur0097@gmail.com>
2020-04-30 10:27:36 -0400

    Created Semester period Api controller

---
+++ BryantUniversity/BryantUniversity/ApiControllers/SemesterPeriodApiController.cs
@@ -0,0 +1,34 @@
+﻿using BryantUniversity.Data;
+using BryantUniversity.Models;
+using BryantUniversity.Repo;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BryantUniversity.ApiControllers
+{
+    [RoutePrefix("api/SemesterPeriod")]
+    [AllowAnonymous]
+    public class SemesterPeriodApiController : ApiController
+    {
+        private Context context;
+
+        public SemesterPeriodApiController()
+        {
+            context = new Context();
+        }
+
+        [Route("{semesterPeriodId}")]
+        [HttpGet]
+        public IHttpActionResult GetSemesterPeriodDetails(int semesterPeriodId)
+        {
+            using (context)
+            {
+                var periodRepo = new SemesterPeriodRepo(context);
+                SemesterPeriod period = periodRepo.GetById(semesterPeriodId);
+
+                return Ok(period);
+            }
+
+        }
+    }
+}
