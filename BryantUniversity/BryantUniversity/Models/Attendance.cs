4322cb3d98d9037c436224f96862eeacb11c598a
Sarthak <sarthakthakur0097@gmail.com>
2020-05-10 00:03:32 -0400

    Implemented Attendance Model

---
+++ BryantUniversity/BryantUniversity/Models/Attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BryantUniversity.Models
+{
+    public class Attendance
+    {
+        public Attendance(){}
+
+        public Attendance(int userId, int courseId, DateTime dateOfClass, bool isPresent)
+        {
+            UserId = userId;
+            CourseId = courseId;
+            DateOfClass = dateOfClass;
+            IsPresent = isPresent;
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public DateTime DateOfClass { get; set; }
+        public bool IsPresent { get; set; }
+    }
+}
