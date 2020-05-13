d0acf9493da1c68dcc549e3bda12d1eb3c366b5b
Sarthak <sarthakthakur0097@gmail.com>
2020-05-12 21:04:22 -0400

    Added View All Majors and Requirements

---
+++ BryantUniversity/BryantUniversity/Controllers/MajorsController.cs
@@ -0,0 +1,35 @@
+﻿using BryantUniversity.Data;
+using BryantUniversity.Models.Repo;
+using BryantUniversity.Repo;
+using BryantUniversity.Security;
+using BryantUniversity.ViewModels;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BryantUniversity.Controllers
+{
+    public class MajorsController : Controller
+    {
+        private Context context;
+
+        public MajorsController()
+        {
+            context = new Context();
+        }
+
+        public ActionResult Index(int id)
+        {
+            MajorRequirementsViewModel viewModel = new MajorRequirementsViewModel();
+            MajorRequirmentsRepo mRepo;
+            using (context)
+            {
+                mRepo = new MajorRequirmentsRepo(context);
+                viewModel.Requirements = mRepo.GetAllMajorRequirementsByMajor(id);
+
+                return View(viewModel);
+            }
+        }
+
+
+    }
+}
