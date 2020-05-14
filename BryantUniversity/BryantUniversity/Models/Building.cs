bb9a15888eaaa69102cde94e1682356ab99d6dd0
Sarthak <sarthakthakur0097@gmail.com>
2020-05-14 10:23:13 -0400

    Removed virtual from room list in building which seemed to fix bug where room would not populate

--- BryantUniversity/BryantUniversity/Models/Building.cs
+++ BryantUniversity/BryantUniversity/Models/Building.cs
@@ -29,7 +29,7 @@ namespace BryantUniversity.Models
         [Required]
         public string BuildingName { get; set; }
         public int Capacity { get; set; }
-        public virtual List<Room> Rooms { get; set; }
+        public List<Room> Rooms { get; set; }
 
     }
 }
